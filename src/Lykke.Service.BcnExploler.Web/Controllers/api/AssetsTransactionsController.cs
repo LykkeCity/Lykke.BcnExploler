d4af488812a7197346988fab6b186acff7ebaa55
some-dev <some-dev@gmail.com>
2017-09-08 18:57:26 +0300

    api/assettransactions/{id} method

--- src/Lykke.Service.BcnExploler.Web/Controllers/api/AssetsTransactionsController.cs
+++ src/Lykke.Service.BcnExploler.Web/Controllers/api/AssetsTransactionsController.cs
@@ -1,39 +1,38 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Threading.Tasks;
-//using System.Web;
-//using System.Web.Http;
-//using BCNExplorer.Web.Models;
-//using Core.Asset;
-//using Core.AssetBlockChanges.Mongo;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Lykke.Service.BcnExploler.Core.Asset;
+using Lykke.Service.BcnExploler.Core.AssetBalanceChanges;
+using Lykke.Service.BcnExploler.Web.Models.Asset;
+using Microsoft.AspNetCore.Mvc;
 
-//namespace BCNExplorer.Web.Controllers.api
-//{
-//    public class AssetsTransactionsController : ApiController
-//    {
-//        private readonly IAssetService _assetService;
-//        private readonly IAssetBalanceChangesRepository _balanceChangesRepository;
+namespace Lykke.Service.BcnExploler.Web.Controllers.api
+{
+    public class AssetsTransactionsController : Controller
+    {
+        private readonly IAssetService _assetService;
+        private readonly IAssetBalanceChangesRepository _balanceChangesRepository;
 
-//        public AssetsTransactionsController(IAssetBalanceChangesRepository balanceChangesRepository, 
-//            IAssetService assetService)
-//        {
-//            _balanceChangesRepository = balanceChangesRepository;
-//            _assetService = assetService;
-//        }
+        public AssetsTransactionsController(IAssetBalanceChangesRepository balanceChangesRepository,
+            IAssetService assetService)
+        {
+            _balanceChangesRepository = balanceChangesRepository;
+            _assetService = assetService;
+        }
 
-//        public async Task<IEnumerable<AssetTransactionViewModel>> Get(string id)
-//        {
-//            var asset = await _assetService.GetAssetAsync(id);
+        [HttpGet("api/assetstransactions/{id}")]
+        public async Task<IEnumerable<AssetTransactionViewModel>> Get(string id)
+        {
+            var asset = await _assetService.GetAssetAsync(id);
 
-//            if (asset != null)
-//            {
-//                var txs = await _balanceChangesRepository.GetTransactionsAsync(asset.AssetIds);
+            if (asset != null)
+            {
+                var txs = await _balanceChangesRepository.GetTransactionsAsync(asset.AssetIds);
 
-//                return txs.Select(AssetTransactionViewModel.Create);
-//            }
+                return txs.Select(AssetTransactionViewModel.Create);
+            }
 
-//            return Enumerable.Empty<AssetTransactionViewModel>();
-//        }
-//    }
-//}
+            return Enumerable.Empty<AssetTransactionViewModel>();
+        }
+    }
+}
